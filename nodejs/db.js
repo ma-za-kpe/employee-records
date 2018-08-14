55a352141ad6d4dca54f31cfc6f8a798907210fc
ma-za-kpe <makpalyy@gmail.com>
2018-08-14 11:04:57 +0300

    create root index.js file with express and body-parser constants

--- nodejs/db.js
+++ nodejs/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+
 mongoose.connect('mongodb://localhost:27017/CRUDE-DB', (err) =>{
   if (!err) {
-    console.log("mongodb connection is successfull")
+    console.log("mongodb connection is successfull...")
   } else {
     console.log("mongodb connection is not successfull: " + JSON.stringify(err, undefined, 2))
   }
