ad340993bff6e7e8bbdce7b6eb13d11ae6581fad
ma-za-kpe <makpalyy@gmail.com>
2018-08-14 11:32:35 +0300

    change db name from CRUDE-DB TO CRUD

--- nodejs/db.js
+++ nodejs/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/CRUDE-DB', (err) =>{
+mongoose.connect('mongodb://localhost:27017/CRUD', (err) =>{
   if (!err) {
     console.log("mongodb connection is successfull...")
   } else {
